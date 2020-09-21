826216a1acf25f3a53ff9b11c8426386f412af53
Alessandro <alessandroadm@live.com>
2020-09-21 20:44:00 -0300

    feat: case insensitive search

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,11 @@ const usersHtml = document.getElementById('results');
 const qtdUsersFound = document.getElementById('qtd-users-found');
 const formPesquisar = document.getElementById('form-pesquisar');
 const resultadoEstatistica = document.getElementById('resultado-estatistica');
+const inputForm = document.getElementById('name');
 let usuariosEncontrado = [];
 
 async function findUsers() {
- usuariosEncontrado = await buscar.default(`https://randomuser.me/api/?seed=javascript&results=70&nat=BR&noinfo`);
+ usuariosEncontrado = await buscar.default(`https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo`);
 }
 
 const renderizar = async (users) => {
@@ -18,11 +19,19 @@ const renderizar = async (users) => {
 const filtrar = (event) => {
  event.preventDefault();
  const name = document.getElementById('name').value;
- const usuarioEncontrados = usuariosEncontrado.filter((user) => user.name.first.includes(name));
+ const usuarioEncontrados = usuariosEncontrado.filter((user) => {
+  
+  if (user.name.first.includes(name) || user.name.last.includes(name)
+  ||user.name.first.includes(name.toUpperCase()) || user.name.last.includes(name.toUpperCase())) {
+   return user;
+  }
+
+ });
  renderizar(usuarioEncontrados);
 };
 
 window.addEventListener('load', () => {
  findUsers();
  formPesquisar.addEventListener('submit', filtrar);
+ inputForm.addEventListener('keyup', filtrar);
 });
